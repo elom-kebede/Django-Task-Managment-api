41cc3f6dba3937acedc7f6a0a019958f3f306457
elom-kebede <kebedeelias30@gmail.com>
2025-03-26 02:42:42 -0800

    create user registeration view

---
+++ ToDo_api/authentication/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from authentication.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(max_length=65, min_length=6, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username','email','password']
+
+    def create(self, validate_data):
+        return User.objects.create_user(**validate_data)
